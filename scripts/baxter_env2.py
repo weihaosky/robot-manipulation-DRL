bc16ddadfa2a11de325cffc62e75e4ff0489a9b6
whyuan <whyuan111@gmail.com>
2018-09-01 20:17:00 +0800

    reward change

--- scripts/baxter_env2.py
+++ scripts/baxter_env2.py
@@ -110,6 +110,7 @@ class Baxter(object):
 
         # ###################### Reset hugging target ##########################
         reset_mode = np.random.choice([1, 2, 4, 5])
+        self.reset_mode = reset_mode
         if reset_mode == 1 or reset_mode == 2:
             self.target_line_start = self.target_line_start - self.target_pos_start
             self.target_pos_start[0] = random.uniform(0.4, 0.8)
@@ -212,14 +213,22 @@ class Baxter(object):
             for idx_robot in range(5, 12):
                 x1_right = self.target_line[idx_target].copy()
                 x2_right = self.target_line[idx_target + 1].copy()
-                x1_right[1] -= 0.15
-                x2_right[1] -= 0.15
+                if self.reset_mode == 1 or self.reset_mode == 2 or self.reset_mode == 4:
+                    x1_right[1] -= 0.15
+                    x2_right[1] -= 0.15
+                if self.reset_mode == 5:
+                    x1_right[1] += 0.15
+                    x2_right[1] += 0.15
                 writhe[idx_target, idx_robot - 5] = GLI(x1_right, x2_right,
                                                         right_limb_pose[idx_robot], right_limb_pose[idx_robot + 1])[0]
                 x1_left = self.target_line[idx_target].copy()
                 x2_left = self.target_line[idx_target + 1].copy()
-                x1_left[1] += 0.15
-                x2_left[1] += 0.15
+                if self.reset_mode == 1 or self.reset_mode == 2 or self.reset_mode == 5:
+                    x1_left[1] += 0.15
+                    x2_left[1] += 0.15
+                if self.reset_mode == 4:
+                    x1_left[1] -= 0.15
+                    x2_left[1] -= 0.15
                 writhe[idx_target, idx_robot - 5 + 7] = GLI(x1_left, x2_left,
                                                             left_limb_pose[idx_robot], left_limb_pose[idx_robot + 1])[0]
                 # writhe[idx_target, idx_robot-5] = \
@@ -236,12 +245,15 @@ class Baxter(object):
                 writhe[idx_target-1, idx_robot-5+7] = \
                     GLI(self.target_line[idx_target], self.target_line[idx_target+1],
                         left_limb_pose[idx_robot], left_limb_pose[idx_robot+1])[0]
-        w_right1 = np.abs(writhe[0:10, 0:7].flatten().sum())
-        w_right2 = np.abs(writhe[0:10, 7:14].flatten().sum())
-        w_left1 = np.abs(writhe[10:20, 0:7].flatten().sum())
-        w_left2 = np.abs(writhe[10:20, 7:14].flatten().sum())
-        w = w_right1 + w_right2 + w_left1 + w_left2
-        reward = (w - w_last) * 50 - 7.5 + w * 5
+        w_right_torso = np.abs(writhe[0:10, 0:7].flatten().sum())
+        w_left_torso = np.abs(writhe[0:10, 7:14].flatten().sum())
+        w_right_arm = np.abs(writhe[10:20, 0:7].flatten().sum())
+        w_left_arm = np.abs(writhe[10:20, 7:14].flatten().sum())
+        if self.reset_mode == 1 or self.reset_mode == 2:
+            w = w_right_torso + w_right_arm + w_left_torso + w_left_arm
+        if self.reset_mode == 4 or self.reset_mode == 5:
+            w = w_right_torso + w_left_torso
+        reward = (w - w_last) * 50 - 5 + w * 5
 
         # Prevent robot arms above humanoid arms
         height_human = self.target_line[4][2] + 0.02
